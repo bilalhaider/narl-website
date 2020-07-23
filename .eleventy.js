0db891937484833f0b48de2e6c963847eef1bd6a
Bilal Haider <bilal.haider@live.com>
2020-07-23 16:46:43 +0500

    Add shortcode for truncating long text

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+
+
+    eleventyConfig.addShortcode("shortened", function (content) {
+
+        const maxLength = 300;
+
+        if(content && typeof content === 'string' && content.length > maxLength) {
+            return content.substring(0, maxLength) + '...';
+        }
+
+        return content;
+    });
+};
