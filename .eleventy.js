63c2e32390e52b100dc130313b9c1cb64c8d3a8d
Bilal Haider <bilal.haider@live.com>
2020-08-05 02:52:01 +0500

    formatted doc

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function (eleventyConfig) {
 
         const maxLength = 300;
 
-        if(content && typeof content === 'string' && content.length > maxLength) {
+        if (content && typeof content === 'string' && content.length > maxLength) {
             return content.substring(0, maxLength) + '...';
         }
 
@@ -13,7 +13,7 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.setTemplateFormats("html,liquid,md,jpg,png");
-    
+
     return {
         dir: {
             input: 'data',
